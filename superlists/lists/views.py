2781e601314ee0bb3e73ed89adee2c623e7ffa0c
rockudaime <bazarow0407@gmail.com>
2016-01-29 18:49:37 +0200

    Add some changes in view and template

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {
+    'new_item_text': request.POST.get('item_text', ''),
+    })
