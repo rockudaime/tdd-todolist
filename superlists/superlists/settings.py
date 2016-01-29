43826f61b1edb9cf6416fb338764971d3cdf7142
rockudaime <bazarow0407@gmail.com>
2016-01-29 15:47:14 +0200

    Refactor home page view to use a template

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'lists',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,3 +82,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
