21069af1975048ad9d2feb7da9d2e9a7f796b1c9
rockudaime <bazarow0407@gmail.com>
2016-01-30 13:44:43 +0200

    New url, view and template to display lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -74,7 +74,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
