dff2970ea8565ae2955f2d8ff31b653d4b52263a
rockudaime <bazarow0407@gmail.com>
2016-01-30 15:12:46 +0200

    Adjusted model so that items are asociated with different lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,7 @@
 #from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from .models import Item
+from .models import Item, List
 
 
 def home_page(request):
@@ -12,5 +12,6 @@ def view_list(request):
     return render(request, 'lists/list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
