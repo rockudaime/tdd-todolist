19a4fa7b7747a1d0253b3ac8c61036453a024073
rockudaime <bazarow0407@gmail.com>
2016-01-30 18:53:53 +0200

    new URL + view for adding to existing lists. FT passes

--- superlists/lists/migrations/0001_initial.py
+++ superlists/lists/migrations/0001_initial.py
@@ -13,10 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('text', models.TextField(default='')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list1',
+            field=models.ForeignKey(default=None, to='lists.List'),
+            preserve_default=True,
         ),
     ]
